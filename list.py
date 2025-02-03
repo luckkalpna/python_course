403ef2b9efff146dbc103c4529562c40b0eca0aa
luckkalpna <luckkalpna@gmail.com>
2025-02-03 20:57:50 +0530

    Lists

---
+++ list.py
@@ -0,0 +1,10 @@
+friends = ["Apple", "Orange", 23, 123.23, "Akash", "Kalpna"]
+
+print(friends)
+# friends[0] = "Mango" # Lists are mutable
+
+# print(friends[0])
+
+friends.append("New Name")
+
+print(friends)
