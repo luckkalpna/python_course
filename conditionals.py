cfadbbb352f14d2d86b4076fff11fcaee00f3aac
luckkalpna <luckkalpna@gmail.com>
2025-02-05 22:40:12 +0530

    If elif else ladded

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age: "))
+
+if(a>=18):
+  print("You are above the age of consent")
+
+elif(a<0):
+  print("You are entered an invalid age")
+
+else:
+  print("You are not above the age of consent")
