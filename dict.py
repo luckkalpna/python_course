386c62c7bf7d71570ba0cbe4bb019135dfdefc75
luckkalpna <luckkalpna@gmail.com>
2025-02-05 22:33:16 +0530

    Dictationary

---
+++ dict.py
@@ -0,0 +1,8 @@
+marks = {
+  "Rohan": 87,
+  "Shubham": 90,
+  "Rahul": 45
+}
+
+print(marks)
+print(marks["Rahul"])  # It is represt tha indexs of any dict...
---
+++ tuple.py
@@ -0,0 +1,7 @@
+a = (1, 34, 345, "Kalpna", "User", False, 56)
+
+print(type(a))
+
+# a[0] = 23  # touples are immutale it can not be changed
+
+print(a)
