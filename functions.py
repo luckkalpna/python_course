5f8a282c772c90737c10a8def494c4e3a65a18fd
luckkalpna <luckkalpna@gmail.com>
2025-03-13 23:53:17 +0530

    Funtions

---
+++ functions.py
@@ -0,0 +1,18 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+# average = (a+b+c)/2
+# print(average)
+
+# function defination
+def avg():
+  a = int(input("Enter your number: "))
+  b = int(input("Enter your number: "))
+  c = int(input("Enter your number: "))
+
+  average = (a+b+c)/2
+  print(average)
+
+avg() # function call
+
