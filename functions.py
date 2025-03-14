7206ae3bdae50edad3e48b321b94562be3fe62c1
luckkalpna <luckkalpna@gmail.com>
2025-03-14 17:11:03 +0530

    Funtions with arguments

--- functions.py
+++ functions.py
@@ -5,14 +5,23 @@
 # average = (a+b+c)/2
 # print(average)
 
-# function defination
-def avg():
-  a = int(input("Enter your number: "))
-  b = int(input("Enter your number: "))
-  c = int(input("Enter your number: "))
+# ----------function defination------------
+# def avg():
+#   a = int(input("Enter your number: "))
+#   b = int(input("Enter your number: "))
+#   c = int(input("Enter your number: "))
 
-  average = (a+b+c)/2
-  print(average)
+#   average = (a+b+c)/2
+#   print(average)
 
-avg() # function call
+# avg() # function call
 
+
+# ------------Functions with arguments---------
+
+def goodDay(name):
+  print("Good Day, " + name)
+
+goodDay("Kalpana")
+goodDay("Anaya")
+goodDay("Bhargavi")
