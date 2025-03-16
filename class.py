18163bdc10f1e238ce3ff9c1b33fd3e62ac70a7b
luckkalpna <luckkalpna@gmail.com>
2025-03-16 21:28:38 +0530

    Intoduction to class in Oops

---
+++ class.py
@@ -0,0 +1,7 @@
+class Employee:
+  name = "Kalpna"
+  language = "Python"
+  salary = 1500000
+
+kalpna = Employee()
+print(kalpna.name, "\n", kalpna.language)
--- file.py
+++ file.py
@@ -3,7 +3,17 @@ program to read any file
 
 '''
 
-f = open("file.txt")
-data = f.read()
-print(data)
+# f = open("file.txt")
+# data = f.read()
+# print(data)
+# f.close()
+
+#-----------Problem_01------------
+f = open("poem.txt")
+content = f.read()
+if("twinkle" in content):
+  print("The word twinkle is present in the content")
+
+else:
+  print("The word twinkle is not present in the content")
 f.close()
