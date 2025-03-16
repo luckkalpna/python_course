3bbb16682db6dfdda77b8a3d0d4e1b3610d8577e
luckkalpna <luckkalpna@gmail.com>
2025-03-16 21:37:19 +0530

    Classes and objects

--- class.py
+++ class.py
@@ -1,7 +1,15 @@
 class Employee:
-  name = "Kalpna"
-  language = "Python"
+  # name = "Kalpna"
+  language = "Python" # This is the class attribute
   salary = 1500000
 
 kalpna = Employee()
-print(kalpna.name, "\n", kalpna.language)
+kalpna.name = "Kalpna" # This is the object attribute
+print(kalpna.name, kalpna.language, kalpna.salary)
+
+
+riya = Employee()
+riya.name = "Riya" # This is the object attribute   
+print(riya.name, kalpna.salary, kalpna.language)
+
+# Here name is object attribute and salary and language are class attributes as they are directly belong to the class
