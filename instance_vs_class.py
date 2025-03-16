44ce48a1a37cdde91f9b61e17bbd9a7cc2edc959
luckkalpna <luckkalpna@gmail.com>
2025-03-16 21:42:06 +0530

    Instance VS Class

---
+++ instance_vs_class.py
@@ -0,0 +1,7 @@
+class Employee:
+  language = "Python"
+  salary = 1500000
+
+kalpna = Employee()
+kalpna.language = "JavaScript" # This is an instance attribute
+print(kalpna.language, kalpna.salary)
