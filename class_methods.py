774366ff4c0e2c64e9cbcc9356950e54c96599e4
luckkalpna <luckkalpna@gmail.com>
2025-03-19 21:25:03 +0530

    Operator overloading

--- class_methods.py
+++ class_methods.py
@@ -4,7 +4,17 @@ class Employee:
   def show(cls):
     print(f"The class attribute of a is {cls.a}")
 
+  @property
+  def name(self):
+    return self.ename
+  
+  @name.setter
+  def name(self, value):
+    self.ename = value
+
 e = Employee()
 e.a = 45
+e.name = "Kalpana"
+print(e.name)
 
 e.show()
